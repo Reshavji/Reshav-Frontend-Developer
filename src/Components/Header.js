0ae0705c75c3f0f2f3d3e8a9b4c3dc7b4b560c05
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-09-16 20:12:15 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -73,7 +73,7 @@ function Header() {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [scrollTimer]);
 
   return (
     <div className={`header-container ${isScrolledUp ? 'scrolled' : ''}`}>
